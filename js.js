8a9e2d6179043ca0d82f44850062d5e80973633e
Louise Ganci Clausen <LouiseGanciClausen@desktop-pd9m040.kea.local>
2017-03-02 09:18:43 +0100

    Add pointer, var for position

--- js.js
+++ js.js
@@ -1,11 +1,26 @@
 var map;
+var ejboparken = {
+    lat: 55.629022,
+    lng: 12.062774
+};
 
 function initMap() {
+    var myLatLng = {
+        lat: 55.629022,
+        lng: 12.062774
+    };
+
+
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: -34.397,
-            lng: 150.644
-        },
-        zoom: 8
+        center: ejboparken,
+
+        zoom: 14,
+        center: myLatLng
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Hello World!'
     });
 }
